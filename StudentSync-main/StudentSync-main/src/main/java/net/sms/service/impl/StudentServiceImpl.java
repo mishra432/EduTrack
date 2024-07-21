a55b59c0e12d0417ef3793434238582d36ff3d83
mishra432 <147365799+mishra432@users.noreply.github.com>
2024-07-21 18:32:06 +0530

    Add files via upload

---
+++ StudentSync-main/StudentSync-main/src/main/java/net/sms/service/impl/StudentServiceImpl.java
@@ -0,0 +1,46 @@
+package net.sms.service.impl;
+
+import java.util.List;
+
+import net.sms.entity.Student;
+import net.sms.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import net.sms.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);	
+	}
+
+}
